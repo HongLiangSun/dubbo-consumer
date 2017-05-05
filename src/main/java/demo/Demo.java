8ec71c32840bdc8bbef44259769bd6b887d2a635
hongliang.sun <hongliang.sun@bestwehotel.com>
2017-05-05 11:01:10 +0800

    整合redis

--- src/main/java/demo/Demo.java
+++ src/main/java/demo/Demo.java
@@ -1,9 +1,13 @@
 package demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import dubbo.service.api.IDemoApi;
@@ -12,11 +16,23 @@ import dubbo.service.api.IDemoApi;
 public class Demo {
 	@Autowired
 	private IDemoApi iDemoApi;
-
+	@Autowired
+	private RedisTemplate<String, String> redisDao;
+	
 	@RequestMapping("/demo")
 	@ResponseBody
-	public String demo(@RequestParam(required = false) String username) {
+	public String demo(String username) {
 		String sayHello = iDemoApi.sayHello(username);
+		Boolean result = redisDao.execute(new RedisCallback<Boolean>() {
+			@Override
+			public Boolean doInRedis(RedisConnection connection) throws DataAccessException {
+				RedisSerializer<String> stringRedisSerializer = redisDao.getStringSerializer();
+				byte[] key = stringRedisSerializer.serialize(username);
+				byte[] value = stringRedisSerializer.serialize(username+"123");
+				return connection.setNX(key, value);
+			}
+		});
+		System.out.println(result);
 		return sayHello;
 	}
 }
